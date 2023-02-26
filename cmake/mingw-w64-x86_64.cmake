77e210f3c23e35446cecd0efd35ac969d10d395c
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-02-26 01:37:32 +0100

    [compilation] + fix compile env on windows (cross compile from mingw)
    /windows-build

--- cmake/mingw-w64-x86_64.cmake
+++ cmake/mingw-w64-x86_64.cmake
@@ -1,4 +1,3 @@
-message(STATUS "Windows build")
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
@@ -6,7 +5,7 @@ set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 set(CMAKE_C_COMPILER /usr/local/bin/x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER /usr/local/bin/x86_64-w64-mingw32-g++)
 # target environment on the build host system
-set(CMAKE_FIND_ROOT_PATH /usr/mingw)
+set(CMAKE_FIND_ROOT_PATH /usr/local/x86_64-w64-mingw32)
 # modify default behavior of FIND_XXX() commands
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
