96cf0db2e968cfbb5dec0742dab495d674e0d33a
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-03-22 09:30:37 +0100

    [CI] ~ fix CI + add more runners
    /packaging

--- docker/gitlab-runner/run_code_quality.sh
+++ docker/gitlab-runner/run_code_quality.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker run -d --name gitlab-runner-code-quality --restart always \
-  -v /home/rom1/Projects/poker-bot/docker/gitlab-runner/config/code-quality:/etc/gitlab-runner \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  gitlab/gitlab-runner:latest
+for i in 1 2 3
+do
+  docker run -d --name gitlab-runner-code-quality-${i} --restart always \
+    -v /home/rom1/Projects/poker-bot/docker/gitlab-runner/config/code-quality-${i}:/etc/gitlab-runner \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    gitlab/gitlab-runner:latest
+done
--- docker/gitlab-runner/run_host.sh
+++ docker/gitlab-runner/run_host.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker run -d --name gitlab-runner-host --restart always \
-  -v /home/rom1/Projects/poker-bot/docker/gitlab-runner/config/host:/etc/gitlab-runner \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  gitlab/gitlab-runner:latest
+for i in 1 2 3
+do
+  docker run -d --name gitlab-runner-host-${i} --restart always \
+    -v /home/rom1/Projects/poker-bot/docker/gitlab-runner/config/host-${i}:/etc/gitlab-runner \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    gitlab/gitlab-runner:latest
+done
