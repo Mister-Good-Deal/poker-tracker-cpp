4bdaa2f8b12a70a954024d19e4e7670787c38757
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-03-22 01:56:44 +0100

    [CI] + add build and tests runners
    /packaging

---
+++ docker/gitlab-runner/register_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Register the runners
+for i in 1 2 3
+do
+  docker run --rm -it \
+    -v /home/rom1/Projects/poker-bot/docker/gitlab-runner/config/tests-${i}:/etc/gitlab-runner \
+    gitlab/gitlab-runner:latest register \
+      --url "https://gitlab.laneuville.me/" \
+      --executor "docker" \
+      --shell "bash" \
+      --docker-image alpine:latest \
+      --description "Tests docker-runner ${i}" \
+      --maintenance-note "Tests docker-runner ${i} launched from docker host machine" \
+      --tag-list "runner,tests" \
+      --clone-url "https://gitlab.laneuville.me/" \
+      --docker-privileged=true \
+      --access-level "not_protected" \
+      --locked=false \
+      --docker-volumes "/cache" \
+      --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+      --registration-token "bqbyyxhqo_WkNWxPHU-N" \
+      --non-interactive
+done
---
+++ docker/gitlab-runner/run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in 1 2 3
+do
+  docker run -d --name gitlab-runner-tests-${i} --restart always \
+    -v /home/rom1/Projects/poker-bot/docker/gitlab-runner/config/tests-${i}:/etc/gitlab-runner \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    gitlab/gitlab-runner:latest
+done
