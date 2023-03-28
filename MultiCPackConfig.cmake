3886b974a9112e21effe4170bbf75c3a89410132
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-03-29 00:09:21 +0200

    [CI] ~ debug
    /packaging

--- MultiCPackConfig.cmake
+++ MultiCPackConfig.cmake
@@ -1,11 +1,11 @@
-if (WINDOWS)
+if (${CPACK_WINDOWS})
     include("build-windows-release/CPackConfig.cmake")
 
     set(CPACK_INSTALL_CMAKE_PROJECTS
         "build-windows-debug;poker-bot;ALL;/"
         "build-windows-release;poker-bot;ALL;/"
         )
-elseif (LINUX)
+elseif (${CPACK_LINUX})
     include("build-linux-release/CPackConfig.cmake")
 
     set(CPACK_INSTALL_CMAKE_PROJECTS
