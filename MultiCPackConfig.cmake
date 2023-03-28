eb271d5cba67d3907ac748661976f44dd188f85b
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-03-28 02:19:44 +0200

    [debug] ~ fix config
    /packaging

--- MultiCPackConfig.cmake
+++ MultiCPackConfig.cmake
@@ -1,13 +1,15 @@
-include("release/CPackConfig.cmake")
-
 if (WIN32)
-    set(CPACK_INSTALL_CMAKE_PROJECTS
-        "build-linux-debug;poker-bot;ALL;/"
-        "build-linux-release;poker-bot;ALL;/"
-        )
-elseif (LINUX)
+    include("build-windows-release/CPackConfig.cmake")
+
     set(CPACK_INSTALL_CMAKE_PROJECTS
         "build-windows-debug;poker-bot;ALL;/"
         "build-windows-release;poker-bot;ALL;/"
+        )
+elseif (LINUX)
+    include("build-linux-release/CPackConfig.cmake")
+
+    set(CPACK_INSTALL_CMAKE_PROJECTS
+        "build-linux-debug;poker-bot;ALL;/"
+        "build-linux-release;poker-bot;ALL;/"
         )
 endif ()
