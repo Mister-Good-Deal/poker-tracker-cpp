bddd4c4268b0d4c65b914d016be636e5f6c07d6a
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-05-26 20:28:09 +0200

    [session] + start service logic

--- services/game_session/include/Session.hpp
+++ services/game_session/include/Session.hpp
@@ -1,15 +1,35 @@
 #pragma once
 
+#include <Game.hpp>
+#include <ScraperFactory.hpp>
+#include <ScraperInterface.hpp>
+
 namespace GameSession {
+    using GameHandler::Game;
+    using Scraper::ScraperInterface;
+    using Scraper::Factory::ScraperFactory;
+    using std::chrono::milliseconds;
+
+    static const uint32_t TICK_RATE = 500;
+
     class Session {
         public:
-            Session()                     = default;
-            Session(const Session& other) = default;
+            Session(std::string_view roomName, uint64_t windowId);
+            Session(const Session& other) = delete;
             Session(Session&& other) noexcept { *this = std::move(other); };
 
             virtual ~Session() = default;
 
-            auto operator=(const Session& other) -> Session& = default;
+            auto operator=(const Session& other) -> Session& = delete;
             auto operator=(Session&& other) noexcept -> Session&;
+
+            auto newGameEvent() -> void;
+
+        private:
+            Game                              _game;
+            std::unique_ptr<ScraperInterface> _scraper;
+            milliseconds                      _tickRate = milliseconds(TICK_RATE);
+            uint64_t                          _windowId = 0;
+            std::string                       _roomName;
     };
 }  // namespace GameSession
--- services/game_session/src/Session.cpp
+++ services/game_session/src/Session.cpp
@@ -1,10 +1,16 @@
 #include "Session.hpp"
 
 namespace GameSession {
+    Session::Session(std::string_view roomName, uint64_t windowId) :
+        _roomName(roomName), _windowId(windowId), _scraper(ScraperFactory::create(_roomName)), _game() {}
+
     auto Session::operator=(Session&& other) noexcept -> Session& {
         if (this != &other)
         {
-            // @todo, prefer using var = std::move(...);
+            _game     = std::move(other._game);
+            _scraper  = std::move(other._scraper);
+            _tickRate = other._tickRate;
+            _windowId = other._windowId;
         }
 
         return *this;
