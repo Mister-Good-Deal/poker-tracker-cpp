49c1f3fcdb2b468616a6722d18aa4048ece90026
Romain Laneuville <romain.laneuville@hotmail.fr>
2023-08-12 23:06:37 +0200

    [Player] - remove hand and position attributes
    The Player class constructor has been modified to use std::string_view instead of std::string for the name parameter. Additionally, several member functions related to hand management and dealer status have been removed. These changes improve efficiency and simplify the code.

--- services/game_handler/include/game_handler/Player.hpp
+++ services/game_handler/include/game_handler/Player.hpp
@@ -8,7 +8,7 @@ namespace GameHandler {
             Player()                        = default;
             Player(const Player& other)     = default;
             Player(Player&& other) noexcept = default;
-            Player(std::string name, uint32_t number, bool self = false) : _name(std::move(name)), _number(number), _self(self){};
+            Player(std::string_view name, uint32_t number, bool self = false) : _name(name), _number(number){};
 
             virtual ~Player() = default;
 
@@ -17,29 +17,19 @@ namespace GameHandler {
             auto operator==(const Player& other) const -> bool { return _name == other._name; }
 
             [[nodiscard]] auto getName() const -> std::string { return _name; }
-            [[nodiscard]] auto getHand() const -> Hand { return _hand; }
             [[nodiscard]] auto getNumber() const -> uint32_t { return _number; }
             [[nodiscard]] auto getStack() const -> int32_t { return _stack; }
-            [[nodiscard]] auto isDealer() const -> bool { return _dealer; }
-            [[nodiscard]] auto isButton() const -> bool { return isDealer(); }  // alias
             [[nodiscard]] auto isEliminated() const -> bool { return _eliminated; }
-            [[nodiscard]] auto self() const -> bool { return _self; }
+            [[nodiscard]] auto isHero() const -> bool { return _number == 1; }  // The player with number 1 is the hero
 
-            auto setHand(const Card& firstCard, const Card& secondCard) -> void { _hand = Hand(firstCard, secondCard); }
-            auto setHand(const Hand& hand) -> void { _hand = hand; }
             auto setStack(int32_t stack) -> void { _stack = stack; }
             auto updateStack(int32_t amount) -> void { _stack += amount; }
-            auto setDealer(bool dealer) -> void { _dealer = dealer; }
-            auto takeButton() -> void { setDealer(true); }
             auto bust() -> void { _eliminated = true; }
 
         private:
             std::string _name;
-            Hand        _hand;
             uint32_t    _number     = 0;
             int32_t     _stack      = 0;
-            bool        _dealer     = false;
-            bool        _self       = false;
             bool        _eliminated = false;
     };
 }  // namespace GameHandler
--- services/game_handler/src/Player.cpp
+++ services/game_handler/src/Player.cpp
@@ -4,11 +4,8 @@ namespace GameHandler {
     auto Player::operator=(Player&& other) noexcept -> Player& {
         if (this != &other) {
             _name       = std::move(other._name);
-            _hand       = std::move(other._hand);
             _number     = other._number;
             _stack      = other._stack;
-            _dealer     = other._dealer;
-            _self       = other._self;
             _eliminated = other._eliminated;
         }
 
